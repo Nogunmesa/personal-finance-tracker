945ec9ee92667de57086b964db3f458f873e6b36
Nogunmesa <nogunmesa@gmail.com>
2023-07-21 13:59:00 -0500

    Latest update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 // src/App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      {/* Add other routes for transactions and setup pages if needed */}
+      {/* <Route path="/transactions" component={Transactions} />
+      <Route path="/setup" component={Setup} /> */}
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
+
 import React, { useState, useEffect } from 'react';
 
 function App() {
@@ -27,3 +44,5 @@ function App() {
 }
 
 export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+
