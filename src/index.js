c5ae7bd1d1cf4e698cb84556fc17348af581e082
Nogunmesa <nogunmesa@gmail.com>
2023-07-21 22:27:34 -0500

    Put everything in correct directory

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import App from './App';
+import App from './app';
 import Transactions from './Transactions';
-import Setup from './Setup'; // Import the Setup component
+import Setup from './setup'; // Import the Setup component
 
 ReactDOM.render(
   <Router>
