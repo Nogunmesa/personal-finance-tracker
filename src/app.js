dea7d322dd5716f2749b4a97dc4e304c52e28048
Nogunmesa <nogunmesa@gmail.com>
2023-07-21 21:14:36 -0500

    Fix the server

--- src/app.js
+++ src/app.js
@@ -17,23 +17,23 @@ ReactDOM.render(
 );
 
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const [accounts, setAccounts] = useState([]);
 
   useEffect(() => {
-    // Fetch account data from the server when the component mounts
-    fetch('/api/accounts')
-      .then(response => response.json())
-      .then(data => setAccounts(data))
-      .catch(error => console.error('Error fetching accounts:', error));
+    // Fetch account data from the server API
+    axios.get('http://localhost:3000/api/accounts')
+      .then((response) => setAccounts(response.data))
+      .catch((error) => console.error('Error fetching accounts:', error));
   }, []);
 
   return (
     <div className="container">
       <h1>Personal Finance Tracker</h1>
       <ul>
-        {accounts.map(account => (
+        {accounts.map((account) => (
           <li key={account.id}>
             {account.accountName} ({account.accountType}): ${account.currentAmount}
           </li>
